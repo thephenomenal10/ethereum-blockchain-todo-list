7e0593a21c0382750c2f2788b6af0e1af41a548c
sahyog saini <42652212+thephenomenal10@users.noreply.github.com>
2020-02-19 19:15:30 +0530

    input box added to create a task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -11,11 +11,18 @@ contract TodoList {
 
     mapping(uint => Task) public tasks;
 
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
     constructor() public{
         createTask("My Todo list of this month");
     }
     function createTask(string memory _content) public {
         taskCount ++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
     }
 }
--- src/app.js
+++ src/app.js
@@ -108,7 +108,13 @@ App = {
         $newTaskTemplate.show()
       }
     },
-  
+    
+    createTask: async () => {
+        App.setLoading(true)
+        const content = $('#newTask').val()
+        await App.todoList.createTask(content)
+        window.location.reload()
+    },
   
     setLoading: (boolean) => {
       App.loading = boolean
