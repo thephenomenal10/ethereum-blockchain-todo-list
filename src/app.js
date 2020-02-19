048e9462a238f47ef4c7e6ecc8403ee75c8a9f7f
sahyog saini <42652212+thephenomenal10@users.noreply.github.com>
2020-02-19 17:38:01 +0530

    account fetched and rendering from the blockchain

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 App = {
+    loading: false,
+    contracts: {},
+
     load: async () => {
         //Load app
         await App.loadWeb3()
         await App.loadAccount()
+        await App.loadContract()
+        await App.render()
     },
 
 // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
@@ -41,6 +46,57 @@ loadWeb3: async () => {
     loadAccount: async () => {
         App.account = web3.eth.accounts[0]
         console.log(App.account)
+    },
+
+    loadContract: async () => {
+        const todoList = await $.getJSON('TodoList.json')
+        // console.log(todoList)
+
+        // create javascript version of smart contracts
+        App.contracts.TodoList = TruffleContract(todoList)
+        App.contracts.TodoList.setProvider(App.web3Provider)
+        console.log(todoList);
+
+        //hydrate the smart contract with values from the blockchain
+        App.todoList = await App.contracts.TodoList.deployed()
+    },
+
+    render: async () => {
+
+        // prevent double render
+        if(App.loading){
+            return
+        }
+        //update app loading state
+        App.setLoading(true)
+
+        //render account
+        $('#account').html(App.account) 
+
+        // update loading state
+        App.setLoading(false)
+    },
+
+    renderTasks: async () => {
+
+        //load the total tasks count from the blockchain
+
+        // render out each task with a new task  template
+
+        //show the task
+    },
+
+    setLoading: (boolean) => {
+        App.loading = boolean
+        const loader = $('#loader')
+        const content = $('#content')
+        if (boolean) {
+            loader.show()
+            content.hide()
+        } else{
+            loader.hide()
+            content.show()
+        }
     }
 
 }
