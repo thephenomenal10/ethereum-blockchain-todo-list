43f99b1da34eb7afdfcb2a05134650a044950333
sahyog saini <42652212+thephenomenal10@users.noreply.github.com>
2020-02-19 16:35:26 +0530

    BlockChain network connected

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ App = {
     load: async () => {
         //Load app
         await App.loadWeb3()
-    }
-}
+        await App.loadAccount()
+    },
 
 // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
 loadWeb3: async () => {
@@ -37,6 +37,13 @@ loadWeb3: async () => {
       console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
     }
   },
+
+    loadAccount: async () => {
+        App.account = web3.eth.accounts[0]
+        console.log(App.account)
+    }
+
+}
 
 
 $(() => {
